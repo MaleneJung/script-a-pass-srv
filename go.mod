e81171c868001c041c91209967c6aab5d4375a01
Madam-JungLe <jung.simon.2002@gmail.com>
2025-06-04 17:42:51 +0200

    Added Frontend Routing

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/MaleneJung/script-a-pass-srv
 
 go 1.24.3
+
+require github.com/MaleneJung/go-mux-frontend-routing-walker v0.0.0-20250604153701-0743a0e75ce4 // indirect
--- main.go
+++ main.go
@@ -1,9 +1,40 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/MaleneJung/go-mux-frontend-routing-walker/frontend"
+)
+
 type Config struct {
 	Port uint16 `json:"port"`
 }
 
 func main() {
+
+	configBuffer, err := os.ReadFile("config.json")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	var config Config
+	if err := json.Unmarshal(configBuffer, &config); err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	serveMux := http.NewServeMux()
+
+	if err := frontend.MuxFrontendWalker(serveMux, "/", "lenes-modular-user/", true); err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	http.ListenAndServe(":"+fmt.Sprint(config.Port), serveMux)
 
 }
